e500797e4c568b8b8eac430157a4a0ade027bcbf
wilson <wilsonhsukdnhc@gmail.com>
2022-11-30 23:14:01 +0800

    select and delete multiple images done.

--- frontend/src/components/createPost.js
+++ frontend/src/components/createPost.js
@@ -2,15 +2,44 @@ import { React, useState, useEffect } from "react";
 import { Button, TextField } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import SearchOnMap from "./SearchOnMap";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
+import trashIcon from "../images/trash.png";
 
 const useStyles = makeStyles(() => ({
   title: {
     color: "yellowgreen",
   },
-  imgPreview: {
-    color: "midnightblue",
+  trashIcon: {
+    width: "20px",
+    height: "20px",
+  },
+  imageSection: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  images: {
+    margin: "1rem .5rem",
+    position: "relative",
+  },
+  imageIndex: {
+    color: "darkslateblue",
+    margin: "0",
+  },
+  deleteButton: {
+    margin: "0",
+    border: "none",
+    borderRadius: "50%",
+    backgroundColor: "white",
+  },
+  indexAndDeleteIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  error: {
+    textAlign: "center",
   },
   layout: {
     margin: "20px",
@@ -23,23 +52,20 @@ const useStyles = makeStyles(() => ({
 
 const CreatePost = () => {
   const classes = useStyles();
+  const [selectedImages, setSelectedImages] = useState([]);
   const [content, setContent] = useState("");
   const [tags, setTags] = useState("");
 
-  const [imgfile, setImgfile] = useState([]);
-  const imgFilehandler = (e) => {
-    if (e.target.files.length !== 0) {
-      setImgfile((imgfile) => [
-        ...imgfile,
-        URL.createObjectURL(e.target.files[0]),
-      ]);
-    }
-  };
+  const onSelectFile = (e) => {
+    const selectedFiles = e.target.files;
+    // console.log(selectedFiles);
+    const selectedFilesArray = Array.from(selectedFiles);
+    // console.log(selectedFilesArray);
+    const imagesArray = selectedFilesArray.map((file) => {
+      return URL.createObjectURL(file);
+    });
 
-  const deleteImage = (id) => {
-    const removeArr = [...imgfile].filter((img) => img.id !== id);
-    setImgfile([removeArr]);
-    console.log("length", imgfile.length);
+    setSelectedImages((previous) => previous.concat(imagesArray));
   };
 
   return (
@@ -49,26 +75,63 @@ const CreatePost = () => {
       </center>
       <div className={classes.layout}>
         <div className={classes.leftLayout}>
-          <div className="picture">
+          <div className="pictureSection">
             <h2>Upload Picture!</h2>
-            <input type="file" onChange={imgFilehandler} />
-            {imgfile.length !== 0 ? (
-              <h2 className={classes.imgPreview}>Preview Your Images</h2>
-            ) : (
-              <></>
-            )}
-            {imgfile.map((file) => {
-              return (
-                <>
-                  <span key={file}>
-                    <img src={file} height="200" width="200" alt="med1" />
+            <input
+              type="file"
+              name="images"
+              onChange={onSelectFile}
+              multiple
+              accept="image/png, image/jpeg, image/webp"
+              style={{ display: "none" }}
+              id="upload-button"
+            />
+            <label htmlFor="upload-button">
+              <Button variant="contained" color="primary" component="span">
+                Upload
+              </Button>
+            </label>
+            {selectedImages.length > 0 &&
+              (selectedImages.length > 10 ? (
+                <p className={classes.error}>
+                  You can't upload more than <b>10</b> images!
+                  <br />
+                  <span>
+                    Please delete <b>{selectedImages.length - 10}</b> images.
                   </span>
-                  <IconButton aria-label="delete" onClick={deleteImage}>
-                    <DeleteIcon />
-                  </IconButton>
-                </>
-              );
-            })}
+                </p>
+              ) : (
+                <></>
+              ))}
+            <div className={classes.imageSection}>
+              {selectedImages &&
+                selectedImages.map((img, index) => {
+                  return (
+                    <div key={img} className={classes.images}>
+                      <img src={img} alt="uploadFile" height="100" />
+                      <div className={classes.indexAndDeleteIcon}>
+                        <p className={classes.imageIndex}>
+                          Picture {index + 1}
+                        </p>
+                        <button
+                          className={classes.deleteButton}
+                          onClick={() =>
+                            setSelectedImages(
+                              selectedImages.filter((e) => e !== img)
+                            )
+                          }
+                        >
+                          <img
+                            className={classes.trashIcon}
+                            src={trashIcon}
+                            alt="trashCanIcon"
+                          />
+                        </button>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
           </div>
           <div className="content">
             <h2>Any Idea?</h2>
