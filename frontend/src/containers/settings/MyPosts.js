88ffc7c3ddd59da54c4065644190345d0128b7c7
wilson <wilsonhsukdnhc@gmail.com>
2023-01-02 20:48:12 +0800

    post details and edit details css done.

--- frontend/src/containers/settings/MyPosts.js
+++ frontend/src/containers/settings/MyPosts.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Home from "../main/Home";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(() => ({
+  mainLayout: {
+    backgroundColor: "aquamarine",
+  },
+}));
 
 export default function MyPosts() {
+  const classes = useStyles();
   return (
-    <Home myPost={true} />
+    <div className={classes.mainLayout}>
+      <Home myPost={true} />
+    </div>
   );
-};
+}
