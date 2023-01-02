69747dcadc63e2b13272d8c5fa4cb9a7c646c9e8
Alex-Lyu0419 <alexlyu0419@gmail.com>
2023-01-02 16:00:00 +0800

    pic don't workgit statusgit status

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -9,74 +9,43 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
-import { makeStyles } from "@mui/styles";
+import { createStyles, makeStyles } from '@mui/styles';
 import { useNavigate } from "react-router-dom";
-import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 // import SearchIcon from '@mui/icons-material/Search';
 
 import { useAuth } from "../hooks/useAuth";
 import { SearchProvider } from "../hooks/useSearch";
-import AccountIcon from "../images/account.png";
-import SavedPostIcon from "../images/savedPost.png";
-import MyPostIcon from "../images/myPost.png";
-import LogoutIcon from "../images/logout.png";
-import HomeIcon from "../images/home.png";
-import WriteIcon from "../images/writing.png";
-import CP3Icon from "../images/cp3.jpg";
-import Search from "../containers/search/Search";
 
-const useStyles = makeStyles(() => ({
-    cp3Icon: {
-        width: "35px",
-        height: "35px",
-    },
-    myAccountIcon: {
-        width: "30px",
-        height: "30px",
-        marginLeft: "-3px",
-        marginRight: "10px",
-    },
-    myPostIcon: {
-        width: "30px",
-        height: "30px",
-        marginRight: "7px",
-    },
-    saveIcon: {
-        width: "30px",
-        height: "30px",
-        marginLeft: "-5px",
-        marginRight: "12px",
-    },
-    logoutIcon: {
-        width: "30px",
-        height: "30px",
-        marginRight: "8px",
-    },
-    homeIcon: {
-        width: "25px",
-        height: "25px",
-        marginRight: "5px",
-    },
-    searchBar: {
-        marginTop: "5px",
-    },
-    writeIcon: {
-        width: "25px",
-        height: "25px",
-        marginRight: "5px",
-    },
-}));
+// const styles = createStyles({
+//     button: {
+//         // main styles,
+//         "&:focus": {
+//             color: "red"
+//         }
+//     }
+// });
 
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        root: {
+            button: {
+                // main styles,
+                "&:focus": {
+                    color: "red"
+                }
+            }
+        },
+    }),
+);
+
+const theme = createTheme();
 
 function AppBar({ pages }) {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const navigate = useNavigate();
     const { user, logout } = useAuth();
-
-    const collapseItems = ["myaccount", "myposts", "savedposts", "logout"];
     const classes = useStyles();
-
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
     };
@@ -87,48 +56,6 @@ function AppBar({ pages }) {
             navigate(path);
         }
     };
-
-    // const Search = styled('div')(({ theme }) => ({
-    //     position: 'relative',
-    //     borderRadius: theme.shape.borderRadius,
-    //     backgroundColor: alpha(theme.palette.common.white, 0.15),
-    //     '&:hover': {
-    //         backgroundColor: alpha(theme.palette.common.white, 0.25),
-    //     },
-    //     marginLeft: 0,
-    //     width: '100%',
-    //     [theme.breakpoints.up('sm')]: {
-    //         marginLeft: theme.spacing(1),
-    //         width: 'auto',
-    //     },
-    // }));
-
-    // const SearchIconWrapper = styled('div')(({ theme }) => ({
-    //     padding: theme.spacing(0, 2),
-    //     height: '100%',
-    //     position: 'absolute',
-    //     pointerEvents: 'none',
-    //     display: 'flex',
-    //     alignItems: 'center',
-    //     justifyContent: 'center',
-    // }));
-
-    // const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    //     color: 'inherit',
-    //     '& .MuiInputBase-input': {
-    //         padding: theme.spacing(1, 1, 1, 0),
-    //         // vertical padding + font size from searchIcon
-    //         paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    //         transition: theme.transitions.create('width'),
-    //         width: '100%',
-    //         [theme.breakpoints.up('sm')]: {
-    //             width: '12ch',
-    //             '&:focus': {
-    //                 width: '20ch',
-    //             },
-    //         },
-    //     },
-    // }));
 
     return (
         <SearchProvider>
@@ -183,19 +110,21 @@ function AppBar({ pages }) {
                                 )}
                             </Menu>
                         </Box>
-                        <Typography
+                        {/* <Typography
                             variant="h6"
                             noWrap
                             component="div"
                             sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
                         >
                             Some Title
-                        </Typography>
+                        </Typography> */}
                         <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
                             {pages?.map((page) => (
                                 <Button
+                                    variant="outlined"
                                     key={page.label}
                                     onClick={() => handleCloseNavMenu(page.path)}
+                                    // style={styles}
                                     sx={{ my: 2, color: "white", display: "block" }}
                                 >
                                     {page.label}
@@ -203,8 +132,10 @@ function AppBar({ pages }) {
                             ))}
                             {!!user && (
                                 <Button
+                                    variant="outlined"
                                     key={"logout"}
                                     onClick={logout}
+                                    // style={styles}
                                     sx={{ my: 2, color: "white", display: "block" }}
                                 >
                                     {"logout"}
--- frontend/src/containers/account/SignUp.js
+++ frontend/src/containers/account/SignUp.js
@@ -1,12 +1,37 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
 import { Link as RouterLink } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import coffeeimg from '../../images/coffee.png';
+
 import { useAuth } from "../../hooks/useAuth";
 import axios from "../../api";
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const theme = createTheme();
 
 const SignUpPage = () => {
     const { signup } = useAuth();
@@ -14,6 +39,11 @@ const SignUpPage = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
+
+        if (data.get("password") !== data.get("confirmpassword")) {
+            return;
+        }
+
         const result = await axios.post('./api/auth/signup', {
             "username": data.get("username"),
             "password": data.get("password"),
@@ -25,58 +55,118 @@ const SignUpPage = () => {
     };
 
     return (
-        <div>
-            <h1>Sign Up</h1>
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="username"
-                    label="User Name"
-                    name="username"
-                    autoComplete="username"
-                    autoFocus
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
+        <ThemeProvider theme={theme}>
+            <Grid container component="main" sx={{ height: '91vh' }} alignItems="center" justifyContent="center">
+                <CssBaseline />
+                {/* <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        // backgroundImage: 'url(https://source.unsplash.com/random)',
+                        backgroundImage: `url(${'../../images/coffee.png'})`,
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                /> */}
+                <Grid
+                    item
+                    xs={12}
+                    sm={8}
+                    md={5}
+                    component={Paper}
+                    elevation={6}
+                    square
+                    sx={{
+                        display: 'center',
+                        flexDirection: 'center',
+                    }}
+                    alignItems="center"
+                    justifyContent="center"
                 >
-                    Sign Up
-                </Button>
-                <Grid container>
-                    <Grid item>
-                        <RouterLink to="/forgetpassword">
-                            <Link href="#" variant="body2">
-                                {"Forget password?"}
-                            </Link>
-                        </RouterLink>
-                    </Grid>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar> */}
+                        <Typography component="h1" variant="h5">
+                            Sign Up
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="username"
+                                label="User Name"
+                                name="username"
+                                autoComplete="username"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="confirmpassword"
+                                label="Confirm Password"
+                                type="password"
+                                id="confirmpassword"
+                                autoComplete="current-password"
+                            />
+                            {/* <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            /> */}
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign Up
+                            </Button>
+                            {/* <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <RouterLink to="/signup">
+                                        <Link href="#" variant="body2">
+                                            {"Don't have an account? Sign Up"}
+                                        </Link>
+                                    </RouterLink>
+                                </Grid>
+                            </Grid> */}
+                            {/* <Copyright sx={{ mt: 5 }} /> */}
+                        </Box>
+                    </Box>
                 </Grid>
-            </Box>
-        </div>
+            </Grid>
+        </ThemeProvider>
     );
-};
+}
 
 export default SignUpPage;
